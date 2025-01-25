cdf4f39d8ec6da6e6529f7a70ba28f6807042758
chuesemann <christoph.huesemann@osnanet.de>
2025-01-25 19:07:44 +0100

    Converter classes moved to DTO

--- rest-api/rest-iot/src/main/java/de/chrhuese/boundary/SoilMoistureDTO.java
+++ rest-api/rest-iot/src/main/java/de/chrhuese/boundary/SoilMoistureDTO.java
@@ -6,4 +6,71 @@ public class SoilMoistureDTO {
     public int percentValue;
     public int sensorId;
     public long timestamp;
+
+
+    public SoilMoistureDTO() {
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public int getRawValue() {
+        return rawValue;
+    }
+
+    public void setRawValue(int rawValue) {
+        this.rawValue = rawValue;
+    }
+
+    public int getPercentValue() {
+        return percentValue;
+    }
+
+    public void setPercentValue(int percentValue) {
+        this.percentValue = percentValue;
+    }
+
+    public int getSensorId() {
+        return sensorId;
+    }
+
+    public void setSensorId(int sensorId) {
+        this.sensorId = sensorId;
+    }
+
+    public long getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(long timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public static class Converter {
+
+        public static SoilMoistureDTO toDTO(SoilMoisture soilMoisture) {
+            SoilMoistureDTO dto = new SoilMoistureDTO();
+            dto.id = soilMoisture.getId();
+            dto.rawValue = soilMoisture.getRawValue();
+            dto.percentValue = soilMoisture.getPercentValue();
+            dto.sensorId = soilMoisture.getSensorId();
+            dto.timestamp = soilMoisture.getTimestamp();
+            return dto;
+        }
+
+        public static SoilMoisture fromDTO(SoilMoistureDTO dto) {
+            SoilMoisture soilMoisture = new SoilMoisture();
+            soilMoisture.setId(dto.getId());
+            soilMoisture.setRawValue(dto.getRawValue());
+            soilMoisture.setPercentValue(dto.getPercentValue());
+            soilMoisture.setSensorId(dto.getSensorId());
+            soilMoisture.setTimestamp(dto.getTimestamp());
+            return soilMoisture;
+        }
+    }
 }
--- rest-api/rest-iot/src/main/java/de/chrhuese/control/TemperatureConverter.java
+++
@@ -1,7 +0,0 @@
-package de.chrhuese.control;
-
-import de.chrhuese.boundary.TemperatureDTO;
-import de.chrhuese.entity.Temperature;
-import jakarta.enterprise.context.ApplicationScoped;
-
-
